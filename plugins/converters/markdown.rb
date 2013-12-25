4e31f41b71dbb0946904d5a66fa0f9447f26523f
Alexander Chernyakhovsky <achernya@mit.edu>
2013-12-24 23:52:50 -0500

    Initial version of the IAP 6.828 website

---
+++ plugins/converters/markdown.rb
@@ -0,0 +1,20 @@
+class Ruhoh
+  module Converter
+    module Markdown
+
+      def self.extensions
+        ['.md', '.markdown']
+      end
+      
+      def self.convert(content)
+        require 'redcarpet'
+        markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:with_toc_data => true),
+          :autolink => true, 
+          :fenced_code_blocks => true,
+	  :tables => true,
+        )
+        markdown.render(content)
+      end
+    end
+  end
+end
